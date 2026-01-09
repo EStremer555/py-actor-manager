a7b5b4136ec2cd95ad6b42cee1d9b069c33d3f1e
Volodymyr Syvak <volodimirsivak388@gmail.com>
2026-01-09 16:23:16 +0200

    sol

--- app/managers.py
+++ app/managers.py
@@ -4,12 +4,12 @@ from app.models import Actor
 
 
 class ActorManager:
-    def __init__(self, db_name: str, table_name: str):
+    def __init__(self, db_name: str, table_name: str) -> None:
         self.db_name = db_name
         self.table_name = table_name
         self.conn = sqlite3.connect(self.db_name)
 
-    def create(self, first_name: str, last_name: str):
+    def create(self, first_name: str, last_name: str) -> Actor:
         self.conn.execute(
             f"INSERT INTO {self.table_name} "
             f"(first_name, last_name) "
@@ -17,13 +17,13 @@ class ActorManager:
             (first_name, last_name)
         )
 
-    def all(self):
+    def all(self) -> Actor:
         actor_cursor = self.conn.execute(
             f"SELECT * FROM {self.table_name}"
         )
         return [Actor(*row) for row in actor_cursor]
 
-    def update(self, pk: int, first_name: str, last_name: str):
+    def update(self, pk: int, first_name: str, last_name: str) -> Actor:
         self.conn.execute(
             f"UPDATE {self.table_name} "
             f"SET first_name=?, last_name=? "
@@ -32,7 +32,7 @@ class ActorManager:
         )
         self.conn.commit()
 
-    def delete(self, pk: int):
+    def delete(self, pk: int) -> Actor:
         self.conn.execute(
             f"DELETE FROM {self.table_name} WHERE pk=?",
             (pk,)
